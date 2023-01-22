10049be070b47771ce1940cff0f7a891b2221c9a
pabloDEVeiga <nwpablodeveloper@gmail.com>
2023-01-22 16:01:12 -0300

    Creando alerts

--- assets/js/alerts.js
+++ assets/js/alerts.js
@@ -1,1 +1,6 @@
-alert( miNombre )
+alert( 'Hola Mundo' );
+let nombre = prompt('¿ Cúal es tu nombre ?', 'Sin nombre');
+console.log( nombre );
+
+const selection = confirm('esta seguro de borrar ?');
+console.log( selection );
