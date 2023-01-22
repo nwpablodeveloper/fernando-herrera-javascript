61603a0f3524b99291b170169f31ddb67c6e6e3e
pabloDEVeiga <nwpablodeveloper@gmail.com>
2023-01-22 18:56:59 -0300

    nombres de variables

---
+++ assets/js/palabras-reservadas.js
@@ -0,0 +1,13 @@
+// let 123variable = 'No se puede iniciar el nombre de la variable con numeros'
+
+// let variable.dfasdf = "no se puede dividir la variable con un punto"
+ 
+// Variables aceptadas
+let _objeto$ = 123;
+let precio99_99 = 236;
+
+let jugadorConMasPuntaje = 'Pablo Veiga';
+
+class JuegoAnio {
+    
+}
