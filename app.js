13c2bfa1a2a947c47c266359d872ec4abe4117f9
pabloDEVeiga <nwpablodeveloper@gmail.com>
2023-01-22 10:18:04 -0300

    Declaración de variables

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+// comentarios con doble barra
 console.log('Hola mundo')
+
+/* forma larga de declarar bariables
+let a = 10;
+let b = 10;
+let c = 10;
+let d = 10;
+let x = a + b;*/
+
+// forma corta de declarar variables
+let a = 10, 
+    b = 20, 
+    c = 20, 
+    d = 25, 
+    x = a + b;
+
+console.log( x );
