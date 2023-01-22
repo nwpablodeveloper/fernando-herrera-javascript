0aad492ae9fe58961b6e56a970e5a2981a67e537
pabloDEVeiga <nwpablodeveloper@gmail.com>
2023-01-22 11:18:23 -0300

    Mostrando variables/valores en consola

--- app.js
+++ app.js
@@ -11,8 +11,31 @@ let x = a + b;*/
 // forma corta de declarar variables
 let a = 10, 
     b = 20, 
-    c = 20, 
-    d = 25, 
+    c = 30, 
+    d = 40, 
+    e = 'Hola ',
+    f = 'Spiderman',
     x = a + b;
 
-console.log( x );
+const saludo = e + f;
+
+// mostrar datos en consola y saber de que variable es
+console.log('%c Mis variables', 'color: green; font-weight: bold');
+console.log( 'a: ', a );
+console.warn( 'b: ', b );
+console.error( 'c: ', c );
+console.info( 'd: ', d);
+
+console.log('  ');
+
+// opción b mostrar datos en consola y saber de que variable es
+console.log('%c Mis variables', 'color: orange; font-weight: bold');
+console.log({ a });
+console.log({ b });
+console.log({ c });
+console.log({ d });
+
+console.log('  ');
+// imprimir variables en una tabla
+console.log('%c Mis variables en una tabla', 'color: blue; font-weight: bold');
+console.table({a, b, c, d, e, f});
