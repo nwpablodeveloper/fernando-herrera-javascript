514e431780bdada5ce51bbe134556296ef6f254d
pabloDEVeiga <nwpablodeveloper@gmail.com>
2023-01-22 16:38:44 -0300

    Datos primitivos - tipo de datos

---
+++ assets/js/primitivos.js
@@ -0,0 +1,32 @@
+let nombre = 'Peter Parker';
+console.log( nombre );
+
+nombre = 'Ben Parker';
+console.log( nombre );
+
+nombre = "Tía May"
+nombre = `Tía May`
+
+console.log( 'Tipo de dato: ', typeof nombre );
+
+nombre = 1231;
+console.log( 'Tipo de dato: ', typeof nombre );
+
+let esMarvel = false;
+console.log( 'Tipo de dato: ', typeof esMarvel );
+
+let edad = 33;
+console.log( 'Tipo de dato: ', typeof edad );
+
+let superPoder;
+console.log( 'Tipo de dato: ', typeof superPoder );
+
+let soyNull = null;
+console.log( 'Tipo de dato: ', typeof soyNull );
+
+let symbol1 = Symbol('a');
+let symbol2 = Symbol('a');
+console.log( 'Tipo de ato: ', typeof symbol1 );
+
+
+console.log( symbol1 === symbol2 );
